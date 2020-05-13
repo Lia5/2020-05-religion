c600467ab90a5492c7f56e8f12a6d45586227e0c
Lia <liaforweb@gmail.com>
2020-05-13 16:27:45 +0300

    desctop

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2020-05-religion
+
+<a href="https://lia5.github.io/2020-05-religion/myapp/index.html">Главная</a>
---
+++ myapp/js/common.js
@@ -0,0 +1,77 @@
+$(function() {
+    $('.js-preloader').preloadinator({
+        minTime: 2000,
+        afterRemovePreloader: function() {
+            $(this).remove();
+            $('body').removeClass('body-modal-open');
+        }
+    });
+   
+    if(jQuery('.scroll-to').length) {
+        var $page = $('html, body');
+        $('.scroll-to[href*="#"]').click(function() {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 400);
+            if ( window.innerWidth < 992 || window.screen.width < 992) {
+                $('.main-menu').removeClass('active');
+                $('.menu-toggle').removeClass('active');
+            }
+            return false;
+        });
+    }
+
+    //animation-text
+    if(jQuery('.letters').length) {
+        $.fn.animate_Text = function() {
+            this.addClass('active');
+            var string = this.text();
+            return this.each(function(){
+                var $this = $(this);
+                $this.html(string.replace(/./g, '<span class="animated">$&</span>'));
+                // $this.find('span.new').each(function(i, el){
+                // setTimeout(function(){ $(el).addClass('fadeIn'); }, 40 * i);
+                // });
+            });
+        };
+        var letters = $('.letters');
+        for (var j=0; j<=letters.length; j++) {
+            $(letters[j]).animate_Text();
+        }
+    }
+
+    //animation
+    setTimeout(function(){  
+            
+        var introLetter = $(".promo-home__title").find('.animated');
+        introLetter.each(function(i,t) {
+            var $this = $(t);
+            setTimeout(function(){ $this.addClass('fadeIn'); },i*20);
+        });
+        
+        var introLetter = $(".animated-parent").find('.animated');
+        introLetter.each(function(i,t) {
+            var $this = $(t);
+            setTimeout(function(){ $this.addClass('fadeIn'); },i*20);
+        });
+
+        var IntroLinks = $(".promo-home__links").find('.animated');
+        IntroLinks.each(function(i,t) {
+            var $this = $(t);
+            setTimeout(function(){ $this.addClass('fadeInUp'); },i*200);
+        });
+
+        var IntroLinks = $(".home__text.animated");
+        IntroLinks.each(function(i,t) {
+            var $this = $(t);
+            setTimeout(function(){ $this.addClass('fadeInUp'); },i*200);
+        });
+        
+    },500);
+
+
+
+
+    
+});
+
